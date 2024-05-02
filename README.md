318b4a0dfdc7e84f0ffbba413aa8b77fc070d9e4
Ivan Oštrić <ivan@halcyon.hr>
2024-05-02 23:37:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# tolerable Discord
+
+A small Firefox extension that rolls back some of the
+[enshittification](https://doctorow.medium.com/social-quitting-1ce85b67b456) currently in progress
+at Discord, mostly aimed at reducing visual clutter and improving performance.
+
+## List of changes
+
+- The Nitro and Shop links are hidden from the `Direct Messages` page.
+- Animated avatar decorations and animated profile effects will no longer appear either in profiles
+  or in the channels.
+- "Discover" and "Download App" buttons are gone from the sidebar.
+- Removed the Nitro gift button that for some perplexing reason appears in every chat box and is
+  absolutely never used.
+- The boost progress bar is permanently disabled for all servers.
+- Disabled promotional banners for annoying games.
+
+## Planned changes
+
+These are not available yet.
+
+- Severely reducing the animations associated with super reacts (leave only the glowing colored
+  reaction badge)
---
+++ tolerable.js
@@ -0,0 +1,71 @@
+/**
+ * Returns a promise that resolves after the element targeted by the selector appears in the DOM.
+ *
+ * @returns {Promise<HTMLElement>}
+ */
+function loader(selector) {
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(_ => {
+      if (document.querySelector(selector)) {
+        observer.disconnect();
+        return resolve(document.querySelector(selector));
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+loader("div#app-mount").then((_) => {
+  console.log("[tolerable] found app element, adding custom css");
+  const stylesheet = document.createElement("style");
+  stylesheet.innerText = `
+
+  /* Hide Nitro and Shop buttons from the sidebar */
+  li[role^="listitem"]:has(div > a[href*="/store"]),
+  li[role^="listitem"]:has(div > a[href*="/shop"]) {
+    display: none;
+  }
+
+  /* Hide animated avatar decorations */
+  img[class*="avatarDecoration"], svg[class*="avatarDecoration"] {
+    display: none;
+  }
+
+  /* Hide animated profile effects */
+  div[class*="profileEffects"] {
+    display: none !important;
+  }
+
+  /* Hide "discover" button and the "download app" button from the sidebar */
+  div[class*="listItem"]:has(* > div[data-list-item-id="guildsnav___guild-discover-button"]),
+  div[class*="listItem"]:has(* > div[data-list-item-id="guildsnav___app-download-button"]) {
+    display: none;
+  }
+
+  /* Hide Nitro gift button from chat bar */
+  button[aria-label="Send a gift"] {
+    display: none;
+  }
+
+  /* Hide boost bar from annoying servers */
+  li:has(> div[data-list-item-id^="channels___boosts-"]) {
+    display: none;
+  }
+
+  /* Hide valorant promo */
+  .container__686c4 {
+    display: none;
+  }
+`
+
+  document.head.appendChild(stylesheet);
+  console.log("[tolerable] css appended");
+});
